6a15e75b4ddae977bfc652750e3d8ae64a669198
Joseph Knox <jknox13@uw.edu>
2018-08-27 11:59:19 -0700

    results from latest run

--- run_test_distance_sample.py
+++ run_test_distance_sample.py
@@ -1,6 +1,5 @@
-# TODO: sample randomly from population keeping distance distribution of ROIs
 from multiprocessing import Pool, cpu_count
-from collections import defaultdict, Counter
+from collections import Counter
 from functools import partial
 import os
 import json
@@ -10,25 +9,23 @@ from scipy import stats
 import numpy as np
 import pandas as pd
 import seaborn as sns
-sns.set_style('white')
 import matplotlib.pyplot as plt
 
 from mcmodels.core import VoxelModelCache
 
 MANIFEST_FILE = os.path.join(os.path.expanduser('~'), 'connectivity',
                              'voxel_model_manifest.json')
-TARGET_SEARCH_SET_ID = 184527634
 ROI_PATH = 'behavior_rois.json'
-OUTPUT_DIR = 'output'
 MODEL_DIR = 'model'
+OUTPUT_DIR = 'output'
+FIGURE_DIR = os.path.join(OUTPUT_DIR, 'figures')
 DISTANCES_PATH = os.path.join(MODEL_DIR, 'distances.csv')
+WEIGHTS_PATH = os.path.join(MODEL_DIR, 'normalized_connection_density.csv')
 
 SUMMARY_STRUCTURES_SET_ID = 687527945
 
 N_SAMPLES = 1000
 TASKS = ('oft', '3-chamber')
-METRICS = ('connection_density', 'connection_strength',
-           'normalized_connection_density', 'normalized_connection_strength')
 
 
 def sample_network(weights, population, network, max_iter=10000, thresh=1, alpha=0.01):
@@ -54,34 +51,34 @@ def sample_network(weights, population, network, max_iter=10000, thresh=1, alpha
     raise RuntimeError('a suitable sample was not found in %d iterations' % max_iter)
 
 
-def get_leaves(tree, acs):
-    acronym_id_map = tree.get_id_acronym_map()
-    return [a for a in acs if len(tree.child_ids([acronym_id_map[a]])[0]) == 0]
-
-
 def get_ss(tree, acs):
     ss = [s['acronym'] for s in tree.get_structures_by_set_id([SUMMARY_STRUCTURES_SET_ID])]
     return list(set(ss).intersection(acs))
 
 
-def plot(samples, test, task, name):
+def get_p_value(samples, test):
     samples = np.log10(samples)
     test = np.log10(test)
 
-    p_value = stats.ttest_1samp(samples, test)[1]
-    pctl = 100 * np.sum(samples < test) / samples.size
+    return stats.ttest_1samp(samples, test)[1]
+
+def plot(samples, test, task):
+    samples = np.log10(samples)
+    test = np.log10(test)
 
     fig, ax = plt.subplots()
+    sns.set_style('white')
     sns.distplot(samples, ax=ax, color=sns.xkcd_rgb['dusky blue'])
-    plt.plot([test, test], [0, ax.get_ylim()[1]], 'r-')
+    plt.plot([test, test], [0, 0.9 * ax.get_ylim()[1]], 'r-')
 
     # remove y
     sns.despine(left=True)
+    ax.set_xlim(-6.5, -3.5)
     ax.set_yticks([])
 
     # label
     ax.set_xlabel('Log normalized connection density')
-    ax.set_title('%s - %s (%.0fth percentile, p-value: %.2e)' % (task, name, pctl, p_value))
+    ax.set_title(task, loc='left')
 
     return fig
 
@@ -99,43 +96,52 @@ def main():
 
     # load distances
     distances = pd.read_csv(DISTANCES_PATH, index_col=0)
+    model = pd.read_csv(WEIGHTS_PATH, index_col=0)
 
-    # TODO: load model
-    path = os.path.join(MODEL_DIR, 'normalized_connection_density.csv')
-    model = pd.read_csv(path, index_col=0)
+    # get level
+    level = get_ss(tree, model.index.values)
 
-    levels = dict(leaves=get_leaves(tree, model.index.values),
-                  ss=get_ss(tree, model.index.values))
-
-    # levels
     for task in TASKS:
-        for name, level in levels.items():
-            net = set(level).intersection(task_rois[task])
-            print(task, name, len(net))
+        net = set(level).intersection(task_rois[task])
 
-            weights = model.loc[level, level].values
-            population = distances.loc[level, level].values
-            network = distances.loc[net, net].values
+        weights = model.loc[level, level].values
+        population = distances.loc[level, level].values
+        network = distances.loc[net, net].values
 
-            valid = np.ix_(~np.isnan(weights).all(axis=1), ~np.isnan(weights).all(axis=0))
-            weights = weights[valid]
-            population = population[valid]
+        valid = np.ix_(~np.isnan(weights).all(axis=1), ~np.isnan(weights).all(axis=0))
+        weights = weights[valid]
+        population = population[valid]
 
-            # run
-            pool = Pool(processes=cpu_count())
-            func = partial(sample_network, weights, population, network,
-                           max_iter=int(1e6), thresh=0.25)
-            mapper = [pool.apply_async(func) for _ in range(N_SAMPLES)]
-            samples = [np.median(f.get()) for f in mapper]
+        # run
+        pool = Pool(processes=cpu_count())
+        func = partial(sample_network, weights, population, network,
+                       max_iter=int(1e6), thresh=0.25)
+        mapper = [pool.apply_async(func) for _ in range(N_SAMPLES)]
+        samples = np.array([np.median(f.get()) for f in mapper])
 
-            test = np.median(model.loc[net, net].values)
-            print("unique graphs: ", Counter(np.unique(samples, return_counts=True)[1]))
+        test = np.median(model.loc[net, net].values)
+        print("unique graphs: ", Counter(np.unique(samples, return_counts=True)[1]))
 
-            # plot
-            fig = plot(samples, test, task, name)
-            fig.savefig(os.path.join(
-                OUTPUT_DIR, 'figures', 'distance_%s_%s.png' % (task, name)))
-            plt.close(fig)
+        # compute stats
+        p_value = get_p_value(samples, test)
+        pctl = 100 * np.sum(samples < test) / samples.size
+
+        # plot
+        fig = plot(samples, test, task)
+        fig.savefig(os.path.join(FIGURE_DIR, '%s_distance_analysis.svg' % task))
+        plt.close(fig)
+
+        # save results
+        result = dict(task=task,
+                      rois=task_rois[task],
+                      p_value=p_value,
+                      percentile=pctl)
+        with open(os.path.join(OUTPUT_DIR, '%s_results' % task), 'w') as f:
+            json.dump(result, f, indent=2)
+
+        # save ROI graph
+        model.loc[level, level].to_csv(
+            os.path.join(MODEL_DIR, '%s_normalized_connection_density.csv') % task)
 
 
 if __name__ == '__main__':
